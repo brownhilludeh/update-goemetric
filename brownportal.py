b138b604c779046cf7c8c9b31230ad92954638dd
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-09-09 18:36:41 +0100

    Update brownportal.py

--- brownportal.py
+++ brownportal.py
@@ -1,20 +1,5 @@
 from turtle import *
 
-
-# pensize(3)
-# pencolor('brown')
-
-
-# def logo():
-#     for i in range(1):
-#         left(45)
-#     for i in range(4):
-#         forward(250)
-#         left(90)
-#         forward(100)
-#         left(90)
-#         forward(100)
-#         left(90)
 def pentagon():
     for i in range(1):
         left(60)
