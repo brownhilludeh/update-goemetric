e3d644d2c1e355b2afe59525cb163c918d699b05
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2021-09-12 10:55:37 +0100

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,31 +1,5 @@
 import random
 
-
-# def rps():
-#     user = input(
-#         "choice? \n'r' for rock. \n's' for scissor \n'p' for paper \n")
-#     comp = random.choice(['r', 'p', 's'])
-
-#     if user == comp:
-#         return "it's a tie"
-
-#     if win(user, comp):
-#         return (f'You won!. Comp chose {comp}')
-
-#     return (f'You lost. Comp chose {comp}')
-
-
-# def win(player, opponent):
-#     if (player == 'r' and opponent == 's') \
-#             or (player == 's' and opponent == 'p') \
-#             or (player == 'p' and opponent == 'r'):
-#         return True
-
-    # return play()
-# print(rps())
-
-    # return play()s
-
 x = "Bry"
 
 def rps():
@@ -41,6 +15,9 @@ def rps():
         return (f'You won!. Comp chose {comp}')
 
     print(f'You lost. Comp chose {comp}')
+    
+#     this file has been modified . feel free to add or remove codes here. 
+# Test question ,,,,, loop the game till a win is declared.... 
 
 
 def win(player, opponent):
